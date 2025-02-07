80acb4d87d0d15874d1f1ece4404855ed877cad4
Dardalorth <dardalorth@carlio.es>
2025-02-07 19:50:22 +0100

    Add Servicio LoggerInterface

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Model\Starship;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,21 +11,39 @@ use Symfony\Component\Routing\Attribute\Route;
 class StarshipApiController extends AbstractController
 {
     #[Route('/api/starships')]
-    public function getCollection(): Response
+    public function getCollection(LoggerInterface $logger): Response
     {
+        dd($logger);
         $starships = [
-            
-            new Starship(1, 'USS LeafyCruiser (NCC-0001)',
-                'Garden', 'Jean-Luc Pickles', 'taken over by Q'),
-           
-            new Starship(2, 'USS Espresso (NCC-1234-C)', 
-                'Latte', 'James T. Quick!', 'repaired'),
-            
-            new Starship(3, 'USS Wanderlust (NCC-2024-W)', 
-                'Delta Tourist', 'Kathryn Journeyway', 'under construction'),
-            
+
+            new Starship(
+                1,
+                'USS LeafyCruiser (NCC-0001)',
+                'Garden',
+                'Jean-Luc Pickles',
+                'taken over by Q'
+            ),
+
+            new Starship(
+                2,
+                'USS Espresso (NCC-1234-C)',
+                'Latte',
+                'James T. Quick!',
+                'repaired'
+            ),
+
+            new Starship(
+                3,
+                'USS Wanderlust (NCC-2024-W)',
+                'Delta Tourist',
+                'Kathryn Journeyway',
+                'under construction'
+            ),
+
         ];
 
         return $this->json($starships);
     }
+
 }
+
