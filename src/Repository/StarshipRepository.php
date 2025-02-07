26bd12095f7c00c583e643c83f8df4903a9ddfd8
Dardalorth <dardalorth@carlio.es>
2025-02-07 20:02:53 +0100

    ADD Repositorio

--- src/Repository/StarshipRepository.php
+++ src/Repository/StarshipRepository.php
@@ -1,21 +1,15 @@
 <?php
 
-namespace App\Controller;
+namespace App\Repository;
 
 use App\Model\Starship;
 use Psr\Log\LoggerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
 
-class StarshipApiController extends AbstractController
+class StarshipRepository
 {
-    #[Route('/api/starships')]
-    public function getCollection(LoggerInterface $logger): Response
+    public function findAll(): array
     {
-        $logger->info('Solicitadas todas las naves espaciales');
-        $starships = [
-
+        return [
             new Starship(
                 1,
                 'USS LeafyCruiser (NCC-0001)',
@@ -23,27 +17,20 @@ class StarshipApiController extends AbstractController
                 'Jean-Luc Pickles',
                 'taken over by Q'
             ),
-
             new Starship(
                 2,
                 'USS Espresso (NCC-1234-C)',
                 'Latte',
                 'James T. Quick!',
-                'repaired'
+                'repaired',
             ),
-
             new Starship(
                 3,
                 'USS Wanderlust (NCC-2024-W)',
                 'Delta Tourist',
                 'Kathryn Journeyway',
-                'under construction'
+                'under construction',
             ),
-
         ];
-
-        return $this->json($starships);
     }
-
 }
-
